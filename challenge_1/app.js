7c00592b99688089c7d4e974bc5164a4c49455d1
Othniel Chan <othnielhr.chan@gmail.com>
2021-11-02 21:22:21 -0500

    rendered board and loaded app.js

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,17 @@
-
-
+// code goes here
+console.log('html rendered');
+// add click handlers
+// document.addEventListener();
+var table = document.getElementById('gameTable');
+console.log(table);
+var cells = document.getElementsByTagName('td');
+console.log(cells);
+for (var cell of cells) {
+  console.log('inside');
+  var thing = cell.getAttribute('id')
+  console.log(thing);
+};
+  // change value when clicked to 'X'
+// add col checkers
+// add row checkers
+// add diag checkers
